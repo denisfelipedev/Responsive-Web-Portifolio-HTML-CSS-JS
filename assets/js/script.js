88d856a809a8c6c2ddc32a5f77719e48e013cac0
Denis Felipe Lopes de Oliviera <denis.choltman@gmail.com>
2021-03-02 18:48:42 -0300

    Iniciando a About Section
    Finalizado o hero e o menu com responsividade.

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $(window).scroll(function(){
+        if(this.scrollY > 20) {
+            $('.navbar').addClass('sticky')
+            $('.logo-color').addClass('logo-white')
+        }
+        else {
+            $('.navbar').removeClass('sticky')
+        }
+    })
+
+    // Toggle menu/navbar script
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass('active')
+        $('.menu-btn i').toggleClass('active')
+    })
+})
